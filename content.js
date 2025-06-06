4febfedaa7871df2ce9c35abcf9cf2b44f09bb76
varun-liver <varun.sureshkumar@outlook.com>
2025-06-05 18:42:53 -0700

    fixed

--- content.js
+++ content.js
@@ -1,4 +1,11 @@
-if (window.location.hostname === "www.example.com"|| window.location.hostname === "example.com") {
+const urls = ["google.com", "example.com"];
+const hostname = window.location.hostname;
+
+if (urls.includes(addwww(hostname)) || urls.includes(hostname)) {
   document.body.style.border = "5px solid red";
-  console.log("at example.com");
+  console.log("✅ Matched:", hostname);
+}
+
+function addwww(str) {
+  return "www." + str;
 }
