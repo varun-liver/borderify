3ff59a1f9bf6d30e23bfd42dce53c4c9414b9918
varun-liver <varun.sureshkumar@outlook.com>
2025-06-09 11:59:08 -0700

    read file

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,18 @@
 // popup.js
 document.getElementById("resetButton").addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.reload(tabs[0].id);
+        const fileInput = document.getElementById('fileInput');
+
+    fileInput.addEventListener('change', (e) => {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        const fileContent = event.target.result;
+        console.log(fileContent);
+      };
+
+      reader.readAsText(file);
+    });
   });
 });
