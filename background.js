492d6491063fdf2327c7562bff65ac1d625b96d7
varun-liver <varun.sureshkumar@outlook.com>
2025-06-09 11:52:54 -0700

    revert

--- background.js
+++ background.js
@@ -1,13 +1,3 @@
-// background.js
-chrome.runtime.onConnect.addListener((port) => {
-  if (port.name === "popup-connection") {
-    console.log("Popup connected!");
-
-    port.onMessage.addListener((msg) => {
-      console.log("Received from popup:", msg);
-
-      // Respond back to popup
-      port.postMessage({ response: "Background received your message!" });
-    });
-  }
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Extension installed!");
 });
--- popup/popup.js
+++ popup/popup.js
@@ -1,20 +1,6 @@
 // popup.js
 document.getElementById("resetButton").addEventListener("click", () => {
-  // 1. Connect to the background
-  const port = chrome.runtime.connect({ name: "popup-connection" });
-
-  // 2. Send a message to background (if needed)
-  port.postMessage({ greeting: "hello from popup" });
-
-  // 3. Listen for a reply
-  port.onMessage.addListener((msg) => {
-    console.log("Got message from background:", msg);
-  });
-
-  // 4. Reload the current active tab
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    if (tabs[0]?.id) {
-      chrome.tabs.reload(tabs[0].id);
-    }
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.reload(tabs[0].id);
   });
 });
